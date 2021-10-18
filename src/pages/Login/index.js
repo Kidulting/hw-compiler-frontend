09cf2f841c419b253fea2667c2188eed3b87c02f
김한성 <khs960616>
2021-10-19 04:43:49 +0900

    feat: [#2] QR 이미지 업로드 컴포넌트 추가

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,3 +1,8 @@
+import "./login.css"
+import QrImgDropDown from "../../components/QrImgDropDown/QrImgDropDown"
+import {qrParser} from '../../utils/qrParser'
+import {hongikQrFormat} from '../../utils/qrParser/constants/hongik'
+
 function Login(){
     const onClickHandler = ()=>{
     /** TODO 서버쪽 로그인 구현 이후 로직에 맞게 처리 하도록 수정 */
@@ -6,10 +11,14 @@ function Login(){
     };
 
     return(
-        <div>
-            로그인 페이지
-            <button onClick={onClickHandler}>로그인 버튼</button>
-        </div>
+        <>
+            <nav className="topbar">HW-COMPILER 로그인 탑바 </nav>
+            <section className='loginContainer'>
+                <div className='loginHeader'>로그인</div>
+                <button onClick={onClickHandler}>라우팅 테스트용 로그인 버튼</button>
+                <QrImgDropDown qrParser={qrParser} dataFormat={hongikQrFormat}/>
+            </section>
+        </>
     )
 }
 
