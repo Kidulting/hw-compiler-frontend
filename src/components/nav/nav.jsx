3c8f2e02f29e4ed791c4aca07b94c059b87287b1
WooYoo <puba5@naver.com>
2021-10-23 16:56:14 +0900

    feat : 네비게이션바 컴포넌트 생성 및 위치 정의

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+function Nav () {
+    return <Wrapper>네비게이션 바입니다</Wrapper>;
+}
+
+const Wrapper = styled.div`
+    width: 200px;
+    height: 100%;
+    border: red 1px solid;
+`
+
+export default Nav;
