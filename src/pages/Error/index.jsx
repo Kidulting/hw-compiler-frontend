d7eae850937749abda814f302c2235d501e5d7d8
puba.yu <puba.yu@kakaocorp.com>
2021-11-12 22:34:23 +0900

    chore : 충돌 해결

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function Error() {
-  return (
-    <div>
-      Error 404 Not found
-    </div>
-  );
+  return <div>Error 404 Not found</div>;
 }
 
 export default Error;
