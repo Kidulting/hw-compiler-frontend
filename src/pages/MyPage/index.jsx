11c701005ccab197f1e492816025ddc165444992
WooYoo <puba5@naver.com>
2021-11-21 19:02:17 +0900

    style : 과제 리스트 임시 생성

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from '../../components/nav/Nav';
+
+const hwList = [
+  { id: '과제1', link: '과제 링크 1' },
+  { id: '과제2', link: '과제 링크 2' },
+  { id: '과제3', link: '과제 링크 3' },
+  { id: '과제4', link: '과제 링크 4' },
+  { id: '과제5', link: '과제 링크 5' },
+];
 
 function MyPage() {
   return (
@@ -7,8 +16,23 @@ function MyPage() {
       <Nav />
       <div>my page</div>
       <div>간편 로그인</div>
+      <div>
+        {hwList.map(hw => (
+          <HWInfo>
+            <div>{hw.id}</div>
+            <div>{hw.link}</div>
+          </HWInfo>
+        ))}
+      </div>
     </div>
   );
 }
+
+const HWInfo = styled.div`
+  border: 2px solid purple;
+  margin: 10px;
+  display: flex;
+  column-gap: 30px;
+`;
 
 export default MyPage;
