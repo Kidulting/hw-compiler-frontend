0724b8c04137bcadcfc88409485b9f870b4cbb89
WooYoo <puba5@naver.com>
2021-11-21 19:08:35 +0900

    style : 주석 추가 및 간편 로그인 레이아웃 작성

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Nav from '../../components/nav/Nav';
 
+// TODO : 임시데이터, 추후 API로 받아오면 삭제할 예정
 const hwList = [
   { id: '과제1', link: '과제 링크 1' },
   { id: '과제2', link: '과제 링크 2' },
@@ -14,23 +15,38 @@ function MyPage() {
   return (
     <div>
       <Nav />
-      <div>my page</div>
-      <div>간편 로그인</div>
-      <div>
-        {hwList.map(hw => (
-          <HWInfo>
-            <div>{hw.id}</div>
-            <div>{hw.link}</div>
-          </HWInfo>
-        ))}
-      </div>
+      <Content>
+        <div>my page</div>
+        <EasyLoginWrapper>간편 로그인</EasyLoginWrapper>
+        <div>
+          {hwList.map(hw => (
+            <HWInfo>
+              <div>{hw.id}</div>
+              <div>{hw.link}</div>
+            </HWInfo>
+          ))}
+        </div>
+      </Content>
     </div>
   );
 }
 
-const HWInfo = styled.div`
+const Content = styled.div`
+  float: left;
+  padding: 10px;
+`;
+
+const EasyLoginWrapper = styled.div`
+  /* TODO : 테두리는 추후 삭제할 예정 */
   border: 2px solid purple;
-  margin: 10px;
+  height: 120px;
+  width: 300px;
+`;
+
+const HWInfo = styled.div`
+  /* TODO : 테두리는 추후 삭제할 예정 */
+  border: 2px solid purple;
+  margin: 10px 0;
   display: flex;
   column-gap: 30px;
 `;
