4bd856785fb328c7cc2a1609bd5e0fbad10c58bc
WooYoo <puba5@naver.com>
2021-11-21 18:29:24 +0900

    style : nav css 속성 수정 및 mypage에 nav 추가

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Nav from '../../components/nav/Nav';
 
 function MyPage() {
-  return <div>my page</div>;
+  return (
+    <div>
+      <Nav />
+      <div>my page</div>
+      <div>간편 로그인</div>
+    </div>
+  );
 }
 
 export default MyPage;
